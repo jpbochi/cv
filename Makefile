c0312eca481555e3fa2de4223e80b5416c66257c
JP Bochi <jpbochi@thoughtworks.com>
2014-02-04 17:25:36 -0200

    make will build a zip file, too

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: clean build_html build_pdf
+all: clean build_zip
 
 install:
 	npm install
@@ -7,10 +7,14 @@ install:
 clean:
 	rm jpbochi.pdf || true
 	rm jpbochi.html || true
+	rm jpbochi.zip || true
 
 build_html: install
 	node ./node_modules/markdown-html/bin/markdown-html.js --style swiss.css README.md > jpbochi.html
 
 build_pdf: install
-	node build jpbochi.pdf
+	node build_pdf jpbochi.pdf
+
+build_zip: build_html build_pdf
+	node build_zip
 
---
+++ build_zip.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var archiver = require('archiver');
+
+var output = fs.createWriteStream(__dirname + '/jpbochi.zip');
+var archive = archiver('zip');
+
+output.on('close', function() {
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+var file1 = __dirname + '/jpbochi.html';
+var file2 = __dirname + '/jpbochi.pdf';
+
+archive
+  .append(fs.createReadStream(file1), { name: 'jpbochi.html' })
+  .append(fs.createReadStream(file2), { name: 'jpbochi.pdf' });
+
+archive.finalize(function(err, bytes) {
+  if (err) {
+    throw err;
+  }
+
+  console.log(bytes + ' total bytes');
+});
